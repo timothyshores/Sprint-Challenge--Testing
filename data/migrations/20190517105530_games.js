715f684f13c34b884ff444a54171bd02c7050e04
Timothy Shores <timothymshores@gmail.com>
2019-05-17 11:09:03 -0500

    Remove name and add title, genre and releaseYear to  data/migrations/20190517105530_games.js

--- data/migrations/20190517105530_games.js
+++ data/migrations/20190517105530_games.js
@@ -1,7 +1,10 @@
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('games', tbl => {
         tbl.increments();
-        tbl.string('name', 128).notNullable().unique();
+        tbl.string('title', 128).notNullable().unique();
+        tbl.string('genre', 128).notNullable();
+        tbl.integer('releaseYear');
+
     });
 };
 
