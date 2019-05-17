1fcaf4082c7144b5b7f7d02a73faa23df73a509a
Timothy Shores <timothymshores@gmail.com>
2019-05-17 12:17:06 -0500

    Add and export getAll function in games/games=model.js and import into server.js with GET request endpoint to /games

--- games/games-model.js
+++ games/games-model.js
@@ -2,9 +2,13 @@ const db = require('../data/dbConfig');
 
 module.exports = {
     insert,
+    getAll
 };
 
 
+function getAll() {
+    return db('games');
+}
 
 async function insert(game) {
     const [id] = await db('games-update').insert(game, 'id');
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const games = require('../games/games-model.js');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.get('/games', async (req, res) => {
+    const rows = await games.getAll();
+
+    res.status(200).json(rows);
+});
 
 module.exports = server;
